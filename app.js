f00c81b6c515d504e8da31c27d7a9f918ac849b6
Mali <lindatrana06@gmail.com>
2024-07-28 19:47:12 -0600

    agregue article

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var letras= 'Creado este archivo'
 function nombre(params) {
   return `${params} D'Trini. `
 }
+
+let nuevoNombre = (a) => {
+
+  if (a=== typeof(String)){
+    console.log('que bueno que eres string')
+  }
+
+}
